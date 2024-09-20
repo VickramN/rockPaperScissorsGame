7c797da3a0230c3cb13dd67c9c4114b56765ff29
VickramN <vickramshiv2004@gmail.com>
2024-09-20 09:09:40 -0400

    added neccessary componenets

--- script.js
+++ script.js
@@ -1,86 +1,146 @@
-function getComputerChoice(){
-    const computerChoice = Math.floor(Math.random()*3)+1;
-    var computer;
-    switch(computerChoice){
-        case 1:
-            computer = "ROCK";
-            break;
-        case 2:
-            computer =  "SCISSORS";
-            break;
-        case 3:
-            computer =  "PAPER";
-            break;
-    }
-    return computer;
-}
-
 let playerScore = 0;
 let computerScore = 0;
 let draw = 0;
+let totalRounds = 0;
+let maxRounds = 3;
+let gameMode = "bestOfThree";
 
-
-
-function playRound(playerSelection, computerSelection){
-    if(playerSelection.toUpperCase() == "ROCK" && computerSelection == "SCISSORS"){
-        playerScore += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":"+ computerScore;
-    } else if(playerSelection.toUpperCase() == "ROCK" && computerSelection == "PAPER"){
-        computerScore += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
-    } else if(playerSelection.toUpperCase() == "ROCK" && computerSelection == "ROCK"){
-        draw += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
-    } else if(playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "ROCK"){
-        computerScore += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
-    } else if(playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "PAPER"){
-        playerScore += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
-    } else if(playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "SCISSORS"){
-        draw += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
-    } else if(playerSelection.toUpperCase() == "PAPER" && computerSelection == "ROCK"){
-        playerScore += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
-    } else if(playerSelection.toUpperCase() == "PAPER" && computerSelection == "SCISSORS"){
-        computerScore += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
-    } else if(playerSelection.toUpperCase() == "PAPER" && computerSelection == "PAPER"){
-        draw += 1;
-        document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
-    }
-
-    if(playerScore == 5){
-        document.getElementById("results").innerHTML = "Congratulations, You won!!!!"
-        playerScore = 0;
-        computerScore = 0;
-        draw = 0;
-    }   else if(computerScore == 5){
-        document.getElementById("results").innerHTML = "The computer won, you suck!!!!"
-        playerScore = 0;
-        computerScore = 0;
-        draw = 0;
-    }
-
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 5) + 1;
+  console.log(computerChoice);
+  var computer;
+  switch (computerChoice) {
+    case 1:
+      computer = "METAL";
+      break;
+    case 2:
+      computer = "WOOD";
+      break;
+    case 3:
+      computer = "WATER";
+      break;
+    case 4:
+      computer = "FIRE";
+      break;
+    case 5:
+      computer = "EARTH";
+      break;
+  }
+  return computer;
 }
 
-document.getElementById("currentScore").innerHTML = playerScore + ":" + draw + ":" + computerScore;
+function setGameMode() {
+  const selectedMode = document.querySelector('input[name="gameMode"]:checked');
+  if (!selectedMode) {
+    console.error("No game mode selected!");
+    return;
+  }
 
-const rockButton = document.getElementById("rockButton");
-rockButton.addEventListener("click", function(){
-    playRound(rockButton.innerHTML,getComputerChoice());
+  const modeValue = selectedMode.value;
+
+  if (modeValue === "3") {
+    maxRounds = 3;
+    gameMode = "bestOfThree";
+  } else if (modeValue === "5") {
+    maxRounds = 5;
+    gameMode = "bestOfFive";
+  } else {
+    maxRounds = Infinity;
+    gameMode = "unlimited";
+  }
+
+  resetGame(); // Reset game whenever the mode changes
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    draw++;
+  } else if (
+    (playerSelection === "METAL" && computerSelection === "WOOD") ||
+    (playerSelection === "METAL" && computerSelection === "EARTH") ||
+    (playerSelection === "WOOD" && computerSelection === "WATER") ||
+    (playerSelection === "WOOD" && computerSelection === "EARTH") ||
+    (playerSelection === "WATER" && computerSelection === "METAL") ||
+    (playerSelection === "WATER" && computerSelection === "FIRE") ||
+    (playerSelection === "FIRE" && computerSelection === "WOOD") ||
+    (playerSelection === "FIRE" && computerSelection === "METAL") ||
+    (playerSelection === "EARTH" && computerSelection === "WATER") ||
+    (playerSelection === "EARTH" && computerSelection === "FIRE")
+  ) {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
+
+  totalRounds += 1;
+
+  document.getElementById("currentScore").innerHTML =
+    playerScore + ":" + draw + ":" + computerScore;
+
+  checkGameEnd();
+}
+
+var delayinMilliseconds = 1500;
+
+function checkGameEnd() {
+  if (gameMode !== "unlimited" && totalRounds >= maxRounds) {
+    endGame();
+  }
+  if (playerScore == Math.ceil(maxRounds / 2)) {
+    document.getElementById("results").innerHTML =
+      "Congratulations, You won!!!!";
+    console.log(delayinMilliseconds);
+    setTimeout(resetGame, delayinMilliseconds);
+  } else if (computerScore == Math.ceil(maxRounds / 2)) {
+    document.getElementById("results").innerHTML =
+      "The computer won, you suck!!!!";
+    setTimeout(resetGame, delayinMilliseconds);
+  }
+}
+
+function endGame() {
+  if (playerScore > computerScore) {
+    document.getElementById("results").innerHTML = "You win the series!";
+  } else if (computerScore > playerScore) {
+    document.getElementById("results").innerHTML =
+      "The computer wins the series!";
+  } else {
+    document.getElementById("results").innerHTML = "It's a draw!";
+  }
+
+  setTimeout(resetGame, delayinMilliseconds);
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  draw = 0;
+  totalRounds = 0;
+  document.getElementById("currentScore").innerHTML =
+    playerScore + ":" + draw + ":" + computerScore;
+  document.getElementById("results").innerHTML = "";
+}
+
+// Event listeners for the buttons
+document.getElementById("metalButton").addEventListener("click", function () {
+  playRound("METAL", getComputerChoice());
 });
 
-const paperButton = document.getElementById("paperButton");
-paperButton.addEventListener("click", function(){
-    playRound(paperButton.innerHTML,getComputerChoice());
+document.getElementById("waterButton").addEventListener("click", function () {
+  playRound("WATER", getComputerChoice());
 });
 
-const scissorsButton = document.getElementById("scissorsButton");
-scissorsButton.addEventListener("click", function(){
-    playRound(scissorsButton.innerHTML,getComputerChoice());
+document.getElementById("woodButton").addEventListener("click", function () {
+  playRound("WOOD", getComputerChoice());
 });
 
+document.getElementById("earthButton").addEventListener("click", function () {
+  playRound("EARTH", getComputerChoice());
+});
 
+document.getElementById("fireButton").addEventListener("click", function () {
+  playRound("FIRE", getComputerChoice());
+});
 
+// Initialize the game mode selection
+setGameMode();
